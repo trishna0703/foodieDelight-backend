3f6589da629905a70de04c188cc21fa74f39f810
Trishna Kashyap <trishna0703@gmail.com>
2024-06-29 21:22:18 +0530

    removed health check

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.get('/', () => ("Server V1.0 updated."))
+// app.get('/', () => ("Server V1.0 updated."))
 app.get("/api/getRestaurantList", getRestaurantList);
 app.post(
   "/api/saveRestaurant",
